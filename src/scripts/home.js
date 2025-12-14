e3a1db97914116999bd5e1329901f1bd7e78865d
oriioll <oriolplazas2@gmail.com>
2025-12-14 23:42:59 +0100

    add footer menu with home, favs, shopping cart and profile

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,4 +1,30 @@
+//Import username from login and register pages and show it
 import {user} from "./auth.js";
 console.log(user);
 
 document.getElementById("greetUser").innerText = user;
+
+//Get all menu SVGs and when they are cliked, change page to that one
+var homeSvg = document.getElementById('homeSvg');
+var favSvg = document.getElementById('favSvg');
+var shoppingCartSvg = document.getElementById('shoppingCartSvg');
+var menuSvg = document.getElementById('menuSvg');
+var profileSvg = document.getElementById('profileSvg');
+
+homeSvg.addEventListener('click', ()=>{
+    window.location.href = "../home";
+})
+
+favSvg.addEventListener('click', ()=>{
+    window.location.href = "../favs";
+})
+
+shoppingCartSvg.addEventListener('click', ()=>{
+    window.location.href = "../cart";
+})
+
+profileSvg.addEventListener('click', ()=>{
+    window.location.href = "../profile";
+})
+
+
